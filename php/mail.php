acb20aed5662603ab04962db5936206c8e5e7ab1
David <dhinojosa@webcreek.com>
2019-03-06 16:25:53 -0500

    Fixed all porfolio items

--- php/mail.php
+++ php/mail.php
@@ -2,12 +2,12 @@
 	$nombre = $_POST['nombre'];
    	$email = $_POST['email'];
 	$mensaje = $_POST['mensaje'];
-	$formcontent="De: $nombre \r\n Mensaje: $mensaje";
-	$recipient= "design@ai-laik.com";
-	$subject="Del formulario de contacto Ai-Laik";
-	$mailheader = "From: $email \r\n";
+	$formcontent="De: $nombre \r\n";
+	$recipient= "info@davidhinojosa.net";
+	$subject="Del formulario de contacto David Hinojosa";
+	$mailheader = "From: $email \r\n  Mensaje: $mensaje";
 
-	mail($recipient, $subject, $formcontent, $mailheader) or die("Parece que algo salio mal. Por favor comunicate con Ai-Laik a design@ai-laik.com");
+	mail($recipient, $formcontent, $mailheader) or die("Parece que algo salio mal. Por favor comunicate con David a info@davidhinojosa.net");
     
     echo "<script>alert('Tu mensaje se envio con exito')</script> ";
     echo "<script>window.location.href='../index.html'</script> ";
